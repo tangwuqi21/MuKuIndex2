519f07a1690fe533b4e9eece6130441007de775e
lmyou <liming951210@163.com>
2020-05-28 11:30:30 +0800

    1、解决送货记录详情接口返回字段缺失问题

--- src/main/java/com/rhdk/purchasingservice/pojo/query/OrderAttachmentQuery.java
+++ src/main/java/com/rhdk/purchasingservice/pojo/query/OrderAttachmentQuery.java
@@ -7,9 +7,7 @@ import lombok.*;
 import java.io.Serializable;
 
 /**
- * <p>
  * 附件表
- * </p>
  *
  * @author LMYOU
  * @since 2020-05-08
@@ -20,23 +18,20 @@ import java.io.Serializable;
 @NoArgsConstructor
 @AllArgsConstructor
 public class OrderAttachmentQuery extends BaseDTO implements Serializable {
-    private static final long serialVersionUID = 1L;
-    @ApiModelProperty(value = "序号", hidden = true)
-    private Long id;
+  private static final long serialVersionUID = 1L;
 
-    @ApiModelProperty(value = "原文件名称")
-    private String orgfilename;
+  @ApiModelProperty(value = "序号", hidden = true)
+  private Long id;
 
-    @ApiModelProperty(value = "文件地址")
-    private String fileurl;
+  @ApiModelProperty(value = "原文件名称")
+  private String orgfilename;
 
-    @ApiModelProperty(value = "描述", hidden = true)
-    private String discription;
+  @ApiModelProperty(value = "文件地址")
+  private String fileurl;
 
-    @ApiModelProperty(value = "附件类型 1-合同附件  2-送货记录附件")
-    private Integer atttype;
+  @ApiModelProperty(value = "附件类型 1-合同附件  2-送货记录附件")
+  private Integer atttype;
 
-    @ApiModelProperty(value = "附件所属父级id", hidden = true)
-    private Long parentId;
-
+  @ApiModelProperty(value = "附件所属父级id", hidden = true)
+  private Long parentId;
 }
--- src/main/java/com/rhdk/purchasingservice/pojo/vo/OrderDelivemiddleVO.java
+++ src/main/java/com/rhdk/purchasingservice/pojo/vo/OrderDelivemiddleVO.java
@@ -1,8 +1,8 @@
 package com.rhdk.purchasingservice.pojo.vo;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import io.swagger.annotations.ApiModelProperty;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,9 +14,7 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * <p>
  * 送货记录明细中间表
- * </p>
  *
  * @author LMYOU
  * @since 2020-05-13
@@ -27,82 +25,89 @@ import java.util.Map;
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class OrderDelivemiddleVO implements Serializable {
-    private static final long serialVersionUID = 1L;
-    @ApiModelProperty(value = "ID")
-    private Long id;
+  private static final long serialVersionUID = 1L;
 
-    @ApiModelProperty(value = "送货记录id")
-    private Long deliveryId;
+  @ApiModelProperty(value = "ID")
+  private Long id;
 
-    @ApiModelProperty(value = "单据号")
-    private String deliverydetailCode;
+  @ApiModelProperty(value = "送货记录id")
+  private Long deliveryId;
 
-    @ApiModelProperty(value = "签收单号")
-    private String signNo;
+  @ApiModelProperty(value = "单据号")
+  private String deliverydetailCode;
 
-    @ApiModelProperty(value = "资产模板id")
-    private Long moduleId;
+  @ApiModelProperty(value = "签收单号")
+  private String signNo;
 
-    @ApiModelProperty(value = "资产模板名称")
-    private String moduleName;
+  @ApiModelProperty(value = "资产模板id")
+  private Long moduleId;
 
-    @ApiModelProperty(value = "属性集合")
-    private String prptIds;
+  @ApiModelProperty(value = "资产模板名称")
+  private String moduleName;
 
-    @ApiModelProperty(value = "属性值集合")
-    private String prptValues;
+  @ApiModelProperty(value = "属性集合")
+  private String prptIds;
 
-    @ApiModelProperty(value = "数量")
-    private Long assetNumber;
+  @ApiModelProperty(value = "属性值集合")
+  private String prptValues;
 
-    @ApiModelProperty(value = "累计金额")
-    private Long totalMoney;
+  @ApiModelProperty(value = "数量")
+  private Long assetNumber;
 
-    @ApiModelProperty(value = "删除标识，0-正常，1-已删除")
-    private Integer delFlag;
+  @ApiModelProperty(value = "累计金额")
+  private Long totalMoney;
 
-    @ApiModelProperty(value = "创建时间")
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
-    private Date createDate;
+  @ApiModelProperty(value = "删除标识，0-正常，1-已删除")
+  private Integer delFlag;
 
-    @ApiModelProperty(value = "创建人")
-    private Long createBy;
+  @ApiModelProperty(value = "创建时间")
+  @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+  private Date createDate;
 
-    @ApiModelProperty(value = "修改时间")
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
-    private Date updateDate;
+  @ApiModelProperty(value = "创建人")
+  private Long createBy;
 
-    @ApiModelProperty(value = "修改人")
-    private Long updateBy;
+  @ApiModelProperty(value = "修改时间")
+  @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+  private Date updateDate;
 
-    @ApiModelProperty(value = "备注")
-    private String remark;
+  @ApiModelProperty(value = "修改人")
+  private Long updateBy;
 
-    @ApiModelProperty(value = "送货单号")
-    private String deliveryCode;
-    @ApiModelProperty(value = "送货单名称")
-    private String deliveryName;
-    @ApiModelProperty(value = "供应商id")
-    private Long supplierId;
-    @ApiModelProperty(value = "供应商名称")
-    private String supplierName;
-    @ApiModelProperty(value = "签收地址")
-    private String signAddress;
-    @ApiModelProperty(value = "合同编号")
-    private String contractCode;
-    @ApiModelProperty(value = "合同名称")
-    private String contractName;
-    @ApiModelProperty(value = "合同类型")
-    private Integer contractType;
+  @ApiModelProperty(value = "备注")
+  private String remark;
 
-    /**
-     * 签收状态（签收状态，0-未签收，1-部分签收，2-已签收）
-     */
-    @ApiModelProperty(value = "签收状态")
-    private Integer signStatus;
+  @ApiModelProperty(value = "送货单号")
+  private String deliveryCode;
 
+  @ApiModelProperty(value = "送货单名称")
+  private String deliveryName;
 
-    @ApiModelProperty(value = "送货明细附件")
-    private List<Map<String,Object>> attachmentList;
+  @ApiModelProperty(value = "供应商id")
+  private Long supplierId;
 
+  @ApiModelProperty(value = "供应商名称")
+  private String supplierName;
+
+  @ApiModelProperty(value = "签收地址")
+  private String signAddress;
+
+  @ApiModelProperty(value = "合同编号")
+  private String contractCode;
+
+  @ApiModelProperty(value = "合同名称")
+  private String contractName;
+
+  @ApiModelProperty(value = "合同类型")
+  private Integer contractType;
+
+  /** 签收状态（签收状态，0-未签收，1-部分签收，2-已签收） */
+  @ApiModelProperty(value = "签收状态")
+  private Integer signStatus;
+
+  @ApiModelProperty(value = "送货明细附件")
+  private List<Map<String, Object>> attachmentList;
+
+  @ApiModelProperty(value = "库管方式（1:量管 2:物管）")
+  private Integer wmType;
 }
