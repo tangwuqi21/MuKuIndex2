8b0dce96075351cdaa613a370a7867bbd538d15a
lmyou <liming951210@163.com>
2020-06-29 13:15:42 +0800

    1、LCN分布式事务整合

--- src/main/java/com/rhdk/purchasingservice/PurchasingserviceApplication.java
+++ src/main/java/com/rhdk/purchasingservice/PurchasingserviceApplication.java
@@ -1,32 +1,28 @@
 package com.rhdk.purchasingservice;
 
+import com.codingapi.txlcn.tc.config.EnableDistributedTransaction;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import javax.servlet.MultipartConfigElement;
-
-
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.rhdk","com.igen"})
+@ComponentScan(basePackages = {"com.rhdk", "com.igen"})
 @MapperScan("com.rhdk.purchasingservice.mapper")
 @EnableEurekaClient
 @EnableAspectJAutoProxy
 @ServletComponentScan
 @EnableSwagger2
 @EnableFeignClients
+@EnableDistributedTransaction
 public class PurchasingserviceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(PurchasingserviceApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(PurchasingserviceApplication.class, args);
+  }
 }
