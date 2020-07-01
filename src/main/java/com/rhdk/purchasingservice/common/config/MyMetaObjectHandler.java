81a4c9b936c1dee113a87a216ee5a331c78e0c5d
lmyou <liming951210@163.com>
2020-07-01 15:51:31 +0800

    1、解决更新时间未发生变化的异常

--- src/main/java/com/rhdk/purchasingservice/common/config/MyMetaObjectHandler.java
+++ src/main/java/com/rhdk/purchasingservice/common/config/MyMetaObjectHandler.java
@@ -10,17 +10,17 @@ import java.util.Date;
 @Component
 public class MyMetaObjectHandler implements MetaObjectHandler {
 
- @Override
- public void insertFill(MetaObject metaObject) {
-  this.strictInsertFill(
-          metaObject, "createBy", Long.class, TokenUtil.getUserInfo().getUserId());
-  this.strictInsertFill(
-          metaObject, "createDate", Date.class, new Date());
- }
+  @Override
+  public void insertFill(MetaObject metaObject) {
+    this.strictInsertFill(metaObject, "createBy", Long.class, TokenUtil.getUserInfo().getUserId());
+    this.strictInsertFill(metaObject, "createDate", Date.class, new Date());
+  }
 
- @Override
- public void updateFill(MetaObject metaObject) {
-  this.strictUpdateFill(metaObject, "updateDate", Date.class, new Date());
-  this.strictUpdateFill(metaObject, "updateBy", Long.class, TokenUtil.getUserInfo().getUserId());
- }
+  @Override
+  public void updateFill(MetaObject metaObject) {
+    metaObject.setValue("updateDate", null);
+    metaObject.setValue("updateBy", null);
+    this.strictUpdateFill(metaObject, "updateDate", Date.class, new Date());
+    this.strictUpdateFill(metaObject, "updateBy", Long.class, TokenUtil.getUserInfo().getUserId());
+  }
 }
