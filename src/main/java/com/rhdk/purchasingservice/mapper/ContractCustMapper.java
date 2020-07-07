ffadfb6a39a877991d1db3da47330e1df7e03b7d
lmyou <liming951210@163.com>
2020-07-07 09:28:42 +0800

    1、增加缺少的mapper文件

---
+++ src/main/java/com/rhdk/purchasingservice/mapper/ContractCustMapper.java
@@ -0,0 +1,15 @@
+package com.rhdk.purchasingservice.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.rhdk.purchasingservice.pojo.entity.ContractCust;
+
+/**
+ * 合同客户 Mapper 接口
+ *
+ * @author YYF
+ * @since 2020-06-12
+ */
+public interface ContractCustMapper extends BaseMapper<ContractCust> {
+
+  // List<Long> getContractCustIds(@Param("id") Long id);
+}
